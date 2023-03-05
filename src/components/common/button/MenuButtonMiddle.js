32ac2c084e722b0919a9e8877e566b49a6411a23
takoyummy <takoyummy@naver.com>
2023-03-06 01:42:29 +0900

    update : change a tag to link tag

--- src/components/common/button/MenuButtonMiddle.js
+++ src/components/common/button/MenuButtonMiddle.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import classNames from 'classnames/bind';
+import { Link } from 'react-router-dom';
 
 /**
  * @description: 메인페이지 중간사이즈 버튼.
@@ -11,9 +12,9 @@ const MenuButtonMiddle = (props) => {
   return (
     <>
       <li className={classNames('main_menu_list', classType)}>
-        <a className="main_menu_list_box" onClick={onClick}>
+        <Link className="main_menu_list_box" onClick={onClick}>
           {text}
-        </a>
+        </Link>
       </li>
     </>
   );
