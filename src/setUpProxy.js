071cb8a7570762a0fac109b8997f23419369bbe1
세아 안 <seahan@kyobobook.com>
2023-07-17 21:52:33 +0900

    update setupProxy.js

--- src/setUpProxy.js
+++ src/setUpProxy.js
@@ -1,10 +1,11 @@
-// const { createProxyMiddleware } = require('http-proxy-middleware');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-// module.exports = function (app) {
-//   app.use(
-//     createProxyMiddleware('/api', {
-//       target: 'http://119.70.42.35:9290',
-//       changeOrigin: true,
-//     }),
-//   );
-// };
+module.exports = function(app) {
+  app.use(
+    '/api',
+    createProxyMiddleware({
+      target: 'http://119.70.42.35:9290',
+      changeOrigin: true
+    })
+  );
+};
